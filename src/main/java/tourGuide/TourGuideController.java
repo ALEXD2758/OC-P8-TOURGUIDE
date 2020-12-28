c874bb6bd12ddecbef821516d5603e93494537ba
Alexandre Dubois <alex_2758@hotmail.com>
2020-12-28 11:58:03 +0100

    Implementation of JavaDoc in TourGuideController Deletion of "getUser" method in TourGuideController as it was redundant with TourGuideService

--- src/main/java/tourGuide/TourGuideController.java
+++ src/main/java/tourGuide/TourGuideController.java
@@ -3,6 +3,7 @@ package tourGuide;
 import java.util.List;
 
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.bind.annotation.RestController;
@@ -19,18 +20,32 @@ public class TourGuideController {
 
 	@Autowired
 	TourGuideService tourGuideService;
-	
-    @RequestMapping("/")
+
+    /** HTML GET request that returns a welcome message
+     *
+     * @return a string message
+     */
+    @GetMapping("/")
     public String index() {
         return "Greetings from TourGuide!";
     }
-    
-    @RequestMapping("/getLocation") 
+
+    /** HTML GET request that returns a random location of the username bounded to the request
+     *
+     * @param userName string of the username (internalUserX)
+     * @return a Json string of a user location in longitude and latitude
+     */
+    @GetMapping("/getLocation")
     public String getLocation(@RequestParam String userName) {
-    	VisitedLocation visitedLocation = tourGuideService.getUserLocation(getUser(userName));
+    	VisitedLocation visitedLocation = tourGuideService.getUserLocation(tourGuideService.getUser(userName));
 		return JsonStream.serialize(visitedLocation.location);
     }
-    
+
+    /** HTML GET request that returns the 5 closest attractions of the username bounded to the request
+     *
+     * @param userName string of the username (internalUserX)
+     * @return a Json string of nearby attractions.
+     */
     //  TODO: Change this method to no longer return a List of Attractions.
  	//  Instead: Get the closest five tourist attractions to the user - no matter how far away they are.
  	//  Return a new JSON object that contains:
@@ -40,18 +55,27 @@ public class TourGuideController {
         // The distance in miles between the user's location and each of the attractions.
         // The reward points for visiting each Attraction.
         //    Note: Attraction reward points can be gathered from RewardsCentral
-    @RequestMapping("/getNearbyAttractions") 
+    @GetMapping("/getNearbyAttractions")
     public String getNearbyAttractions(@RequestParam String userName) {
-    	VisitedLocation visitedLocation = tourGuideService.getUserLocation(getUser(userName));
+    	VisitedLocation visitedLocation = tourGuideService.getUserLocation(tourGuideService.getUser(userName));
     	return JsonStream.serialize(tourGuideService.getNearByAttractions(visitedLocation));
     }
-    
-    @RequestMapping("/getRewards") 
+
+    /** HTML GET request that returns the rewards of the username bounded to the request
+     *
+     * @param userName string of the username (internalUserX)
+     * @return a Json string of all UserRewards
+     */
+    @GetMapping("/getRewards")
     public String getRewards(@RequestParam String userName) {
-    	return JsonStream.serialize(tourGuideService.getUserRewards(getUser(userName)));
+    	return JsonStream.serialize(tourGuideService.getUserRewards(tourGuideService.getUser(userName)));
     }
-    
-    @RequestMapping("/getAllCurrentLocations")
+
+    /** HTML GET request that returns the current location of all users
+     *
+     * @return a Json string of current location of all users.
+     */
+    @GetMapping("/getAllCurrentLocations")
     public String getAllCurrentLocations() {
     	// TODO: Get a list of every user's most recent location as JSON
     	//- Note: do not use gpsUtil to query for their current location,
@@ -65,14 +89,15 @@ public class TourGuideController {
     	
     	return JsonStream.serialize("");
     }
-    
-    @RequestMapping("/getTripDeals")
+
+    /** HTML GET request that returns 5 random trip deals of the username bounded to the request
+     *
+     * @param userName string of the username (internalUserX)
+     * @return a string of a list of Provider in a random way
+     */
+    @GetMapping("/getTripDeals")
     public String getTripDeals(@RequestParam String userName) {
-    	List<Provider> providers = tourGuideService.getTripDeals(getUser(userName));
+    	List<Provider> providers = tourGuideService.getTripDeals(tourGuideService.getUser(userName));
     	return JsonStream.serialize(providers);
-    }
-    
-    private User getUser(String userName) {
-    	return tourGuideService.getUser(userName);
     }
 }
